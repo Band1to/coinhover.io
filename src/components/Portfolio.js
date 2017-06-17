bfce1795a8c57de2d42043da01a8137ce32c9842
leongaban <leongaban@gmail.com>
2017-06-17 13:54:15 -0500

    added fontello fonts

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,39 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import CoinModal from './CoinModal'
 
 class Portfolio extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {
-			coin_modal_open: false
-		};
+		this.state = {};
 	}
 
 	render() {
-		let modalOpen = this.state.coin_modal_open;
-
-		const addCoin = () => {
-			console.log('addCoin...');
-			console.log(' modalOpen', !modalOpen);
-			this.setState({
-        		coin_modal_open: !modalOpen
-        	});
-		};
-
 		return (
 			<div className="app-bg">
-				{ modalOpen && <CoinModal/> }
 				<section className="welcome">
 					<header>					
 						<h1>CoinHover</h1>
-						<h2>Watch all your balances</h2>
+						<h2>Watch all your cryptocurrency balances</h2>
+						<p>Currently in development...</p>
 					</header>
-					<section className="coin-table" onClick={ addCoin }>
-						<p>Add Coin</p>
-						<div className="ico-add"></div>
-						<div className="coin-input"></div>
-					</section>
+					<footer>
+						<ul>
+							<li>
+								<div className="icon-github"></div>
+							</li>
+							<li>
+								<div className="icon-twitter"></div>
+							</li>
+							<li>
+								<div className="icon-facebook-squared"></div>
+							</li>
+						</ul>
+					</footer>
 				</section>
 			</div>
 		)
