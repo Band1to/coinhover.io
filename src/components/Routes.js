0f9618d6fb92e9884b673b9eabb2a29bbc45f30a
leongaban <leongaban@gmail.com>
2017-07-07 08:38:56 -0500

    Link from react-router-dom used to fix broken path to /portfolio online

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Portfolio from './portfolio/Portfolio'
+import Home from './home/Home'
 import NoMatch from './NoMatch'
 
 const Routes = () => {
     return (
         <Switch>
-        	<Route exact={ true } path="/" component={ Portfolio }/>
+        	<Route exact={ true } path="/" component={ Home }/>
+        	<Route exact={ true } path="/portfolio" component={ Portfolio }/>
         	<Route component={ NoMatch } />
         </Switch>
     );
