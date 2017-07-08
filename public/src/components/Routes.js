78dd60cda9fd2510855f4d9ec05aae1c28f837e8
leongaban <leongaban@gmail.com>
2017-07-07 20:06:56 -0500

    BrowserRouter fixed in Routes.js

--- public/src/components/Routes.js
+++ public/src/components/Routes.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { browserHistory, BrowserRouter, hashHistory, Route, Switch } from 'react-router-dom'
 import Portfolio from './portfolio/Portfolio'
 import Home from './home/Home'
 import NoMatch from './NoMatch'
 
 const Routes = () => {
     return (
-        <Switch>
-        	<Route exact={ true } path="/" component={ Home }/>
-        	<Route exact={ true } path="/portfolio" component={ Portfolio }/>
-        	<Route component={ NoMatch } />
-        </Switch>
+    	<BrowserRouter history={ browserHistory }>
+	        <Switch>
+	        	<Route exact={ true } path="/" component={ Home }/>
+	        	<Route exact={ true } path="/portfolio" component={ Portfolio }/>
+	        	<Route component={ NoMatch } />
+	        </Switch>
+        </BrowserRouter>
     );
 }
 
