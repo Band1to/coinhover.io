f856b5c86f9455a798d6453397b6587755c9cb92
leon-yum <leon.gaban@yum.com>
2017-09-17 15:03:49 -0500

    fixed import

--- app/config/Routes.js
+++ app/config/Routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { browserHistory, HashRouter, Route, Switch } from 'react-router-dom';
-import { Portfolio } from 'containers';
-import { Home } from 'containers';
-import { NoMatch } from 'containers';
+import { Portfolio } from '../containers';
+import { Home } from '../containers';
+import { NoMatch } from '../containers';
 
 const Routes = () => (
   <HashRouter history={browserHistory}>
